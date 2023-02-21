c88dce630cf8ad040c3ff114e4444c61de8af3e7
rbnicodev <rubenmsanchis.dev@gmail.com>
2023-02-21 17:50:26 +0100

    supress warnings

--- product-microservice/src/main/java/com/rbnico/productmicorservice/entity/ListResponse.java
+++ product-microservice/src/main/java/com/rbnico/productmicorservice/entity/ListResponse.java
@@ -4,25 +4,24 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.domain.Pageable;
 
-import java.util.ArrayList;
 import java.util.List;
 @Getter
 @Setter
 public class ListResponse<T> {
 
-    private List<T> results = new ArrayList<>();
+    private List<T> results;
     private Integer pageSize = 0;
     private Integer page = 0;
     private String sortBy = "";
 
-    public ListResponse (List results, Pageable paging) {
+    public ListResponse (List<T> results, Pageable paging) {
         this.results = results;
         this.page = paging.getPageNumber();
         this.pageSize = paging.getPageSize();
         this.sortBy = paging.getSort().toString();
     }
 
-    public ListResponse (List results) {
+    public ListResponse (List<T> results) {
         this.results = results;
     }
 
